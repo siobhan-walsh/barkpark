e4c55d7f9cd0ea3b390e1f54a0091d1b39b4760f
siobhan-walsh <siobhanemmarose@gmail.com>
2015-05-05 22:22:24 -0700

    working on search functionality

--- js/app.js
+++ js/app.js
@@ -6,12 +6,10 @@ $(document).ready(
 
 	function(){
 		
-                var clicked = false;
+          
                 
             //for the mobile image navigation feedback
-            
-       
-                
+             
                 $("#mc").click(
                     function(){
                         $("#mc img").css({"opacity":"1"});
@@ -40,78 +38,31 @@ $(document).ready(
                 
          
 		
-		var clicked = false;
+		
              
 			 
 			   
             //for the search funtion
-                $("#search").keypress(function(e){
+            
+                $("#search").blur(function(){
+                     
+                    $("#webinfo").css("opacity", "1");
                     
-                    var term = $(".search").val();
                     
-                    console.log("ITS PRESSED");
-                    
-                    if(term.startsWith('m')){
-                        
-                        $("#webinfo").css("opacity", "0.4");
-                        $("#sresults").show().html(
-                            '<a href="mccartney.html"><div class=" thumb small-6 columns">'+
-                                '<img src="img/mccartney-thumb.jpg">'+
-                            '</div>'+
-                            '<div class="thumbinfo small-6 left columns">'+
-                                '<h5>McCartney Creek Park</h5>'+
-                                '<h6 class="subheader">Larkhall Crescent</h6>'+
-                                '<i class="fi-paw"></i>'+
-                                '<i class="fi-paw"></i>'+
-                                '<i class="fi-paw"></i>'+
-                                '<i class="grey fi-paw"></i>'+
-                                '<i class="grey fi-paw"></i><br>'+
-                                '<p class="ilabel">4/5 paws</p>'+
-                            '</div></a>'
-                       );
-                        
-                        console.log("ITS AN MM");
-                    } else if(term.startsWith('s')){
-                        
-                        $("#webinfo").css("opacity", "0.4");
-                        $("#sresults").show().html(
-                            '<div class=" thumb small-6 columns">'+
-                                '<img src="img/seylynn-thumb.jpg">'+
-                            '</div>'+
-                            '<div class="thumbinfo small-6 left columns">'+
-                                '<h5>Seylynn Park</h5>'+
-                                '<h6 class="subheader">Mountain Highway</h6>'+
-                                '<i class="fi-paw"></i>'+
-                                '<i class="fi-paw"></i>'+
-                                '<i class="fi-paw"></i>'+
-                                '<i class="fi-paw"></i>'+
-                                '<i class="grey fi-paw"></i><br>'+
-                                '<p class="ilabel">4/5 paws</p>'+
-                            '</div>'+
-                            '<div class=" thumb small-6 columns">'+
-                                '<img src="img/stanley-thumb.jpg">'+
-                            '</div>'+
-                            '<div class="thumbinfo small-6 left columns">'+
-                                '<h5>Stanley Park</h5>'+
-                                '<h6 class="subheader">Lagoon Drive</h6>'+
-                                '<i class="fi-paw"></i>'+
-                                '<i class="fi-paw"></i>'+
-                                '<i class="grey fi-paw"></i>'+
-                                '<i class="grey fi-paw"></i>'+
-                                '<i class="grey fi-paw"></i><br>'+
-                                '<p class="ilabel">2/5 paws</p>'+
-                            '</div>'
-                       );
-                        
-                        console.log("ITS AN MM");
-                    } else {
-                        console.log("THAT AIN'T NO M");
-                        $("#webinfo").css("opacity", "0.4");
-                        
+                });
+                
+                
+                $(".search").keyup(function(){
+                
+                        searching();
                     }
-                    
-                }
                 );
+        
+                
+        
+        
+                
+                
             //for the mobile dropdown menu toggley
 		$('#men').click( 
 		
@@ -140,5 +91,74 @@ $(document).ready(
                     $("#plusmin").toggleClass('fi-plus fi-minus');
                 }
             );
+    
+    var searching = function(){
+                    var term = $(".search").val();
+                    
+                    console.log("ITS PRESSED");
+                    
+                    if(term.indexOf('m') == 0){
+                        
+                        $("#webinfo").css("opacity", "0.4");
+                        $(".sresults").css("display","block").html(
+                            '<a href="mccartney.html"><div class=" thumb large-4 small-6 columns">'+
+                                '<img src="img/mccartney-thumb.jpg">'+
+                            '</div>'+
+                            '<div class="thumbinfo large-6 small-6 left columns">'+
+                                '<h5>McCartney Creek Park</h5></a>'+
+                                '<h6 class="subheader">Larkhall Crescent</h6>'+
+                                '<i class="fi-paw"></i>'+
+                                '<i class="fi-paw"></i>'+
+                                '<i class="fi-paw"></i>'+
+                                '<i class="grey fi-paw"></i>'+
+                                '<i class="grey fi-paw"></i><br>'+
+                                '<p class="ilabel">4/5 paws</p>'+
+                            '</div>'
+                       );
+                        
+                        console.log("ITS AN MM");
+                    } else if(term.indexOf('s') == 0){
+                        
+                        $("#webinfo").css("opacity", "0.4");
+                        $(".sresults").show().html(
+                            '<div class=" thumb small-6 large-4 columns">'+
+                                '<img src="img/seylynn-thumb.jpg">'+
+                            '</div>'+
+                            '<div class="thumbinfo small-6 left columns">'+
+                                '<h5>Seylynn Park</h5>'+
+                                '<h6 class="subheader">Mountain Highway</h6>'+
+                                '<i class="fi-paw"></i>'+
+                                '<i class="fi-paw"></i>'+
+                                '<i class="fi-paw"></i>'+
+                                '<i class="fi-paw"></i>'+
+                                '<i class="grey fi-paw"></i><br>'+
+                                '<p class="ilabel">4/5 paws</p>'+
+                            '</div>'+
+                            '<div class=" thumb small-6 large-4 columns">'+
+                                '<img src="img/stanley-thumb.jpg">'+
+                            '</div>'+
+                            '<div class="thumbinfo small-6 left columns">'+
+                                '<h5>Stanley Park</h5>'+
+                                '<h6 class="subheader">Lagoon Drive</h6>'+
+                                '<i class="fi-paw"></i>'+
+                                '<i class="fi-paw"></i>'+
+                                '<i class="grey fi-paw"></i>'+
+                                '<i class="grey fi-paw"></i>'+
+                                '<i class="grey fi-paw"></i><br>'+
+                                '<p class="ilabel">2/5 paws</p>'+
+                            '</div>'
+                       );
+                        
+                        console.log("ITS AN MM");
+                    } else if(term.indexOf() == -1){
+                        console.log("THAT AIN'T NO M");
+                        $("#webinfo").css("opacity", "0.4");
+                        $(".sresults").css("display","none")
+                        console.log(term)
+                        console.log(term.indexOf('m'));
+                        
+                    }
+                }
+    
         }				
 );  
